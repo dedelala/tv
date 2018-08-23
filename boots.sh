0da9632b4ac410daddc0bb43470d0e59dbf5f21b
Dede Lamb <dede.lamb@myob.com>
2018-08-24 09:54:34 +1000

    add snd conf

--- boots.sh
+++ boots.sh
@@ -10,10 +10,9 @@ ln -s /etc/sv/socklog-unix /var/service/ || die "sv socklog-unix"
 ln -s /etc/sv/nanoklogd /var/service/ || die "sv nanoklogd"
 ln -s /etc/sv/docker /var/service/ || die "sv docker"
 
-useradd -G wheel,floppy,audio,video,cdrom,optical,kvm,xbuilder,socklog,docker \
-  -m -d /home/dede -s /bin/zsh dede || die "user dede"
-
 useradd -G audio,video,docker -m -d /home/x -s /bin/zsh x || die "user x"
+useradd -G wheel,audio,video,kvm,xbuilder,socklog,docker,x \
+  -m -d /home/dede -s /bin/zsh dede || die "user dede"
 
 s=/home/dede/src
 git clone https://github.com/dedelala/tv.git "$s/tv" || die "tv.git"
@@ -21,6 +20,7 @@ cp -R "$s/tv/conf/"* / || die "tv conf"
 
 chown -R dede:dede /home/dede || die "chown dede"
 chown -R x:x /home/x || die "chown user x"
+chmod 770 /home/x || die "chmod user x home"
 
 su -c "$s/tv/build.sh" dede || die "build.sh"
 mv "$s/kakoune/src/kak.debug" /usr/bin/kak || die "no kak"
