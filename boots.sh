83260680d04605a6363ff9691ff2049058ef6445
Dede Lamb <dede.lamb@myob.com>
2018-08-23 18:43:08 +1000

    clean up somewhatw

--- boots.sh
+++ boots.sh
@@ -2,8 +2,6 @@
 
 die() { echo "oh noes: $* sad face"; exit 1; }
 
-[[ -e tvoid.zip ]] || die "missing tvoid.zip package"
-
 xbps-install -Suyy || die "update"
 xbps-install -Syy socklog-void linux-firmware alsa-libs docker fortune-mod git zsh \
   unzip zip xorg-minimal xf86-video-nouveau xhost xinit xorg-fonts xrandr || die "pkgs"
@@ -17,13 +15,17 @@ useradd -G wheel,floppy,audio,video,cdrom,optical,kvm,xbuilder,socklog,docker \
 
 useradd -G audio,video,docker -m -d /home/x -s /bin/zsh x || die "user x"
 
-unzip tvoid.zip -d / || die "unzip package"
+s=/home/dede/src
+git clone https://github.com/dedelala/tv.git "$s/tv" || die "tv.git"
+cp -R "$s/tv/conf/"* / || die "tv conf"
+
 chown -R dede:dede /home/dede || die "chown dede"
 chown -R x:x /home/x || die "chown user x"
 
-su -c /home/dede/boots.sh dede || die "dede boots"
-mv /home/dede/src/kakoune/src/kak.debug /usr/bin/kak || die "no kak"
-mv /home/dede/src/dwm/dwm /usr/bin/dwm || die "no dwm"
-mv /home/dede/src/st/st /usr/bin/st || die "no st"
+su -c "$s/tv/build.sh" dede || die "build.sh"
+mv "$s/kakoune/src/kak.debug" /usr/bin/kak || die "no kak"
+mv "$s/dwm/dwm" /usr/bin/dwm || die "no dwm"
+mv "$s/st/st" /usr/bin/st || die "no st"
 
 ln -s /etc/sv/x /var/service/ || die "sv x"
+
--- build.sh
+++ build.sh
@@ -3,31 +3,36 @@
 die() { echo "awww: $* not work soz"; exit 1; }
 
 s="$HOME/src"
-mkdir "$s" || die "src dir"
 cd "$s" || die "cd src"
+if ! [[ -d dots ]]; then
+    git clone https://github.com/dedelala/dots.git || die "get dots"
+    ./dots/dots.sh || die "dots.sh"
+fi
 
-git clone https://github.com/dedelala/dots.git || die "get dots"
-./dots/dots.sh || die "dots.sh"
-
-git clone https://github.com/dedelala/tv.git || die "get tv"
 for i in fox snd kakao sucka; do
     cd "$s/tv/$i" || die "cd $i"
     docker build -t "$i:latest" . || die "build $i"
 done
 cd "$s" || die "cd src"
 
-git clone https://git.suckless.org/dwm || die "get dwm"
-cd dwm || die "cd dwm"
-git apply "$s/tv/dwm-start.patch" || die "patch dwm"
-cd "$s" || die "cd src"
+if ! [[ -d dwm ]]; then
+    git clone https://git.suckless.org/dwm || die "get dwm"
+    cd dwm || die "cd dwm"
+    git apply "$s/tv/dwm-start.patch" || die "patch dwm"
+    cd "$s" || die "cd src"
+fi
 docker run -it --rm -v "$s/dwm:/src" -w /src \
   sucka:latest sh -c "make clean && make"
 
-git clone https://git.suckless.org/st || die "get st"
+if ! [[ -d st ]]; then
+    git clone https://git.suckless.org/st || die "get st"
+fi
 docker run -it --rm -v "$s/st:/src" -w /src \
   sucka:latest sh -c "make clean && make" || die "build st"
 
-git clone https://github.com/mawww/kakoune.git || die "get kakoune"
+if ! [[ -d kakoune ]]; then
+    git clone https://github.com/mawww/kakoune.git || die "get kakoune"
+fi
 docker run -it --rm -v "$s/kakoune:/src" -w /src/src \
   kakao:latest sh -c "make clean && make" || die "build kakoune"
 
